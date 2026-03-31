20e13976b0dbd8490652e418c3169ffc6f411f76
Rubén Vargas <159955249+Ruco141@users.noreply.github.com>
2026-03-31 08:19:26 -0600

    Add About Us and Partners

---
+++ src/app/about/page.js
@@ -0,0 +1,6 @@
+export const metadata = {
+    title: "Nosotros — Looppah",
+};
+
+export default function AboutUsPage() {
+}
---
+++ src/app/partners/page.js
@@ -0,0 +1,6 @@
+export const metadata = {
+    title: "Socios — Looppah",
+};
+
+export default function PartnersPage() {
+}
