29e53800f5a96effad245c5328d02071447b1577
Rubén Vargas <159955249+Ruco141@users.noreply.github.com>
2026-03-31 08:40:56 -0600

    Update Next.js configuration for image quality and allowed development origins

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        qualities: [75, 90],
+    },
+    allowedDevOrigins: ['192.168.100.103'],
 };
 
 export default nextConfig;
